3df30b23d2630167a965f0b47f8ce6f222f0f8d5
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-24 14:26:42 -0700

    Started adding elements and animations using framer-motion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { PageContext } from "../context/PageContext";
+import "./Home.css";
+
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../utils/animations";
 
 const Home = () => {
-  const { hello, setHello } = React.useContext(PageContext);
-  return <div>{hello}</div>;
+  const pageContext = React.useContext(PageContext);
+  return (
+    <AnimatePresence>
+      {pageContext.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejsLogo.png"
+              alt="threejs logo"
+              className="threejsLogo"
+            />
+          </motion.header>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Home;
