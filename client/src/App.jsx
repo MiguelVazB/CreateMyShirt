7b0883f6cc223d409ee2941268d699ff64c5814c
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-23 18:48:02 -0700

    Setting up project
    Setting up the pages and the canvas to be used by three.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <h1>Create My Shirt</h1>
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
