3cfe550648901f8f1c10980583769cf78fe4ab6b
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-26 16:48:53 -0700

    Started adding the initial layout of the customizer page

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { PageContext } from "../context/PageContext";
+import "./Customizer.css";
+
+import {
+  fadeAnimation,
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../utils/animations";
 
 const Customizer = () => {
-  return <div>Customizer</div>;
+  const pageContext = useContext(PageContext);
+  return (
+    <AnimatePresence>
+      {!pageContext.intro && (
+        <>
+          <motion.div key="customize" {...slideAnimation("left")}>
+            <div className="custom-tab">
+              <div>Color Changer</div>
+              <div>Logo Changer</div>
+              <div>AI Helper</div>
+            </div>
+          </motion.div>
+
+          <motion.div {...fadeAnimation}>
+            <button
+              className="goBackButton"
+              onClick={() => pageContext.setIntroFunc(true)}
+            >
+              Go Back
+            </button>
+          </motion.div>
+
+          <motion.div {...slideAnimation("up")}>
+            <div className="customToggles">
+              <div>Color Changer</div>
+              <div>Logo Changer</div>
+              <div>AI Helper</div>
+            </div>
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Customizer;
