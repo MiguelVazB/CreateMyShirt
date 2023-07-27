a7727531c3cf043480111553984c1b17cee7628f
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-27 15:28:48 -0700

    Added the 3D model
    I can now see the 3D model but, it is really small. I need to create and modify the CameraRig component to zoom into it

--- src/canvas/Shirt.jsx
+++ src/canvas/Shirt.jsx
@@ -1,12 +1,27 @@
 import React, { useContext } from "react";
 import { easing } from "maath";
-import PageContext from "../context/PageContext";
+import { PageContext } from "../context/PageContext";
 import { useFrame } from "@react-three/fiber";
 import { Decal, useGLTF, useTexture } from "@react-three/drei";
 
 const Shirt = () => {
   const pageContext = useContext(PageContext);
-  return <div>Shirt</div>;
+  const { nodes, materials } = useGLTF("./tshirt.glb");
+
+  const logoTexture = useTexture("./threejsLogo.png");
+  const fullTexture = useTexture("./threejsLogo.png");
+
+  return (
+    <group>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      ></mesh>
+    </group>
+  );
 };
 
 export default Shirt;
