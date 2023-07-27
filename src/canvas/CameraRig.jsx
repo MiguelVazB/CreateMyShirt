d11dc1dbaf0ab388762cd5b4715dcedf6c44dbe0
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-27 15:43:32 -0700

    Added necessary imports to the CameraRig
    Also, I added passing children as props. This follows Three.js rules

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { PageContext } from "../context/PageContext";
 
-const CameraRig = () => {
-  return <div>CameraRig</div>;
+const CameraRig = ({ children }) => {
+  const pageContext = useContext(PageContext);
+
+  const groupRef = useRef(null);
+
+  return <group ref={groupRef}>{children}</group>;
 };
 
 export default CameraRig;
