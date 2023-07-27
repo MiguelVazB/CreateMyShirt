fcf3ed81cbd3785cd1c20ed6ceadd54de40dfc7d
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-27 16:51:50 -0700

    Added logic based on the context to display the textures

--- src/canvas/Shirt.jsx
+++ src/canvas/Shirt.jsx
@@ -19,7 +19,27 @@ const Shirt = () => {
         material={materials.lambert1}
         material-roughness={1}
         dispose={null}
-      ></mesh>
+      >
+        {pageContext.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          ></Decal>
+        )}
+        {pageContext.isLogoTexture && (
+          <Decal
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            anisotropy={16}
+            depthTest={false}
+            depthWrite={true}
+          ></Decal>
+        )}
+      </mesh>
     </group>
   );
 };
