5261ab0370b5c4f260435c18bc7e7147baa91d1e
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-26 17:57:08 -0700

    Added initial setup and necessary dependencies to create the canvas with the 3D model

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+import Shirt from "./Shirt";
+import CameraRig from "./CameraRig";
+import Background from "./Background";
 
-const Canvas = () => {
-  return <div>Canvas</div>;
+const MainCanvas = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      {/* <CameraRig> */}
+      {/* <Background /> */}
+      <Center>
+        <Shirt />
+      </Center>
+      {/* </CameraRig> */}
+    </Canvas>
+  );
 };
 
-export default Canvas;
+export default MainCanvas;
