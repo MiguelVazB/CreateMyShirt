ec0dffceb7c35c9519aff427a110fdae67e54d85
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-30 01:51:32 -0700

    Implemented a file uploader

--- src/components/FileChanger.jsx
+++ src/components/FileChanger.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import readFileContent from "../utils/FileReader";
+import { PageContext } from "../context/PageContext";
 
 const FileChanger = () => {
-  return <div>FileChanger</div>;
+  const pageContext = useContext(PageContext);
+
+  const handleFileSelect = (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      readFileContent(selectedFile, ({ url }) => {
+        pageContext.setLogoTexture(url);
+      });
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        style={{ display: "none" }}
+        onChange={handleFileSelect}
+      />
+      <button
+        onClick={() => document.querySelector('input[type="file"]').click()}
+      >
+        Click to upload file
+      </button>
+    </div>
+  );
 };
 
 export default FileChanger;
