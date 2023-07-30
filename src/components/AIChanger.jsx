c9a292fb4aa61c3a6b4e0452e6978c18d11e52ab
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-29 17:49:41 -0700

    Created the other two components that change the shirt

---
+++ src/components/AIChanger.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AIChanger = () => {
+  return <div>AIChanger</div>;
+};
+
+export default AIChanger;
---
+++ src/components/FileChanger.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FileChanger = () => {
+  return <div>FileChanger</div>;
+};
+
+export default FileChanger;
