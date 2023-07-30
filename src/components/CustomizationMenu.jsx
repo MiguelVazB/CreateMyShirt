2375dfd6416abf38f31bbd1290e89418168e85d4
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-07-29 17:50:17 -0700

    Added logic to display the selected changer component

--- src/components/CustomizationMenu.jsx
+++ src/components/CustomizationMenu.jsx
@@ -1,15 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { PageContext } from "../context/PageContext";
+import ColorChanger from "./ColorChanger";
+import FileChanger from "./FileChanger";
+import AIChanger from "./AIChanger";
 import Tab from "./Tab";
 import "./CustomizationMenu.css";
 
 const CustomizationMenu = () => {
   const pageContext = useContext(PageContext);
+
+  const [activeTab, setActiveTab] = useState();
+  const [file, setFile] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [generatingImage, setGeneratingImage] = useState(false);
+
+  const generateTabContent = () => {
+    switch (activeTab) {
+      case "colorChanger":
+        return <ColorChanger />;
+      case "fileChanger":
+        return <FileChanger />;
+      case "aiChanger":
+        return <AIChanger />;
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div className="customTab">
-      <Tab tab="Color" />
-      <Tab tab="File" />
-      <Tab tab="AI" />
+    <div className="customMenu">
+      <div className="customTab">
+        <Tab tab="Color" handleClick={() => setActiveTab("colorChanger")} />
+        <Tab tab="File" handleClick={() => setActiveTab("fileChanger")} />
+        <Tab tab="AI" handleClick={() => setActiveTab("aiChanger")} />
+      </div>
+      {generateTabContent()}
     </div>
   );
 };
