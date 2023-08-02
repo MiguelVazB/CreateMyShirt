8042c793e21e2d45e79f5bc153a75f2fd55295a6
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-08-01 17:59:49 -0700

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -7,12 +7,12 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit: "50mb"}))
+app.use(express.json({limit: '50mb'}));
 
-app.use('/api/dalle', dalleRoutes)
+app.use('/api/v1/dalle', dalleRoutes)
 
 app.get('/', (req, res) => {
     res.status(200).json({message: "Hello World!"})
 })
 
-app.listen(5000, () => console.log("Server has started on port 5000"))
+app.listen(8080, () => console.log("Server has started on port 8080"))
