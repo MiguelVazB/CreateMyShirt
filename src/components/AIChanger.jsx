ef080e79d2b73b28fceabd652a0ba399f5633b04
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-08-01 18:02:59 -0700

    Added structure of the AI changer

--- src/components/AIChanger.jsx
+++ src/components/AIChanger.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const AIChanger = () => {
-  return <div>AIChanger</div>;
+const AIChanger = ({ prompt, setPrompt, generatingImage, handleSubmit }) => {
+  return (
+    <div className="aiChanger">
+      <textarea
+        className="aiChangerTextArea"
+        placeholder="Ask AI for a design..."
+        rows={10}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <div className="aiButtons">
+        {generatingImage ? (
+          <button>Asking AI...</button>
+        ) : (
+          <>
+            <button onClick={() => handleSubmit("logo")}>AI Logo</button>
+            <button onClick={() => handleSubmit("full")}>AI Full</button>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AIChanger;
