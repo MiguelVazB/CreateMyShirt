ed301f461fb3ee712a16c68d8ba2b78fa5593aad
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-08-02 17:48:57 -0700

    Added alt text in case the image does not load

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Tab = ({ tab, isActive, handleClick }) => {
+const Tab = ({ tab, handleClick }) => {
   return (
     <div className="tab" onClick={handleClick}>
-      <img src={`${tab}.png`} />
+      <img src={`${tab}.png`} alt={`${tab} changer`} />
     </div>
   );
 };
