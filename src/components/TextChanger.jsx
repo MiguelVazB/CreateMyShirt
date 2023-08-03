2abdaab2fe13dea5decb9cc28bd114f172fe51d8
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-08-03 14:51:55 -0700

    Fixed the Font Picker
    The font picker works properly

--- src/components/TextChanger.jsx
+++ src/components/TextChanger.jsx
@@ -1,9 +1,14 @@
-import React, { useRef, useContext } from "react";
+import React, { useRef, useContext, useState, useEffect } from "react";
 import { PageContext } from "../context/PageContext";
+import FontPicker from "font-picker-react";
 
 const TextChanger = () => {
   const pageContext = useContext(PageContext);
   const inputRef = useRef(null);
+  const [currentFont, setCurrentFont] = useState("Open Sans");
+
+  const [apiKey, setApiKey] = useState("");
+  const [apiKeyReady, setApiKeyReady] = useState(false);
 
   function handleClick() {
     const canvas = document.createElement("canvas");
@@ -11,19 +16,24 @@ const TextChanger = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     canvas.style.backgroundColor = "transparent";
     let fontSize = 50;
-    ctx.font = `${fontSize}px Arial`;
+    ctx.font = `${fontSize}px ${currentFont}`;
     const text = inputRef.current.value;
     const textWidth = ctx.measureText(text).width;
     const x = (canvas.width - textWidth) / 2;
     const y = (canvas.height + fontSize) / 2;
     ctx.fillText(text, x, y);
     const dataUrl = canvas.toDataURL();
-    pageContext.setLogoTexture({
-      logo: dataUrl,
-      logoName: "Text",
-    });
     pageContext.setTextOverlay(dataUrl);
+    pageContext.setIsTextOverlay(true);
   }
+
+  useEffect(() => {
+    setApiKey(import.meta.env.VITE_API_KEY);
+  }, []);
+
+  useEffect(() => {
+    if (apiKey) setApiKeyReady(true);
+  }, [apiKey]);
 
   return (
     <div className="textChanger">
@@ -34,6 +44,14 @@ const TextChanger = () => {
         type="text"
         placeholder="Enter text..."
       />
+      {apiKeyReady && (
+        <FontPicker
+          apiKey={apiKey}
+          activeFontFamily={currentFont}
+          onChange={(nextFont) => setCurrentFont(nextFont.family)}
+        />
+      )}
+      {/* <input type="range" /> */}
       <button onClick={handleClick}>Set Text</button>
     </div>
   );
