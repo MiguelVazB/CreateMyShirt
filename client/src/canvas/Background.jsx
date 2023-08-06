aaf4f08488ef85d733f4d95681e19d71b0b65c27
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2023-08-06 03:33:13 -0700

    decrease the amount of RandomizedLights

--- client/src/canvas/Background.jsx
+++ client/src/canvas/Background.jsx
@@ -1,6 +1,4 @@
 import React, { useRef } from "react";
-import { easing } from "maath";
-import { useFrame } from "@react-three/fiber";
 import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 
 const Background = () => {
@@ -17,14 +15,14 @@ const Background = () => {
       rotation={[Math.PI / 2, 0, 0]}
     >
       <RandomizedLight
-        amount={4}
+        amount={1}
         radius={9}
         intensity={0.9}
         ambient={0.25}
         position={[5, 10, -10]}
       />
       <RandomizedLight
-        amount={4}
+        amount={1}
         radius={5}
         intensity={0.8}
         ambient={0.5}
