9ac69a5bf3624bf0e7cf41e0e822e1d11e69347a
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2026-01-10 15:23:51 -0800

    feat: enhance Canvas performance settings and rendering options

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -12,9 +12,13 @@ const MainCanvas = () => {
       className="mainCanvas"
       shadows
       camera={{ position: [0, 0, 0], fov: 30 }}
+      dpr={[1, 2]}
+      performance={{ min: 0.5 }}
       gl={{
         preserveDrawingBuffer: false,
         useLegacyLights: false,
+        antialias: true,
+        powerPreference: "high-performance",
       }}
     >
       <ambientLight intensity={0.5} />
