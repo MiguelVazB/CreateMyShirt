4e597849c204bdc11835f418fe4347829b36526c
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2026-01-10 16:32:55 -0800

    feat: add isActive prop to Tab component for dynamic styling

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Tab = ({ tab, handleClick }) => {
+const Tab = ({ tab, handleClick, isActive }) => {
   return (
-    <div className="tab" onClick={handleClick} title={`${tab} changer`}>
+    <div 
+      className={`tab ${isActive ? 'tab-active' : ''}`} 
+      onClick={handleClick} 
+      title={`${tab} customization`}
+    >
       <img src={`${tab}.png`} alt={`${tab} changer`} />
+      <span className="tab-label">{tab}</span>
     </div>
   );
 };
